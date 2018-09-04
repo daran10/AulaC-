69b0f05e1bb7f128f32e2fa09f79cdc54b8ee9ae
daran10 <36827107+daran10@users.noreply.github.com>
2018-09-04 20:38:14 -0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Objetos
+{
+    class Pessoa
+    {
+        //Atributos
+        private string nome;
+        private int idade;
+        private double altura;
+        private double peso;
+        private string cpf;
+
+        // Método Construtor
+        public Pessoa()//1º construtor
+        {
+            this.nome = "";
+            this.idade = 0;
+            this.altura = 0.0;
+            this.peso = 0;
+            this.cpf = "";
+        }
+
+        public Pessoa(string cpf, string nome)
+        {
+        this.cpf = cpf;
+            this.nome = nome;
+        }
+
+    //Metodos
+    public string NomeCPF()
+    {
+    return this.nome +"-" + this.cpf;
+    }
+
+    }//fim da classe Pessoa
+}//fim do namespace Objetos
