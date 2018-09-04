fc6c8746791053800006e5d35124994d69142cf1
daran10 <36827107+daran10@users.noreply.github.com>
2018-09-04 20:36:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            Pessoa pessoa2 = new Objetos.Pessoa("05555555", "Domingos");
+
+            Console.WriteLine(pessoa2.NomeCPF());
+
+             Console.WriteLine("\n\nPressione qualquer tecla para sair");
+             Console.ReadKey(true);
+
+        }//fim do metodo main
+    }//fim da classe program
+}//fim do namespace Objetos
